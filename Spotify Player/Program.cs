5e4a116b3eb01a19c3ee577a5d201491477ea685
Pedra96 <darkraidialga@yahoo.it>
2022-12-11 00:06:25 +0100

    inizio implementazione try catch e sanificazione input

--- Spotify Player/Program.cs
+++ Spotify Player/Program.cs
@@ -1,9 +1,10 @@
 ﻿
 using Spotify_Player;
 
-Brano Brano = new("Toxicity", 180, "System of a down", new List<string> { "Metal", "Rock", "Punk" });
-Podcast Podcast = new("Lorem Ipsum", new List<string> { "Marco", "Giovanni", "Giorgio" }, "Sev Stez","Hello World", 254, new List<string> { "Comicità", "Fiction", "Musica", "Just Talking" });
-
+Brano Brano = new("Toxicity", 180, "System of a down", new List<string> { "Nu metal" });
+Podcast Podcast = new("Lorem Ipsum", new List<string> { "Marco", "Giovanni", "Giorgio" }, "Sev Stez","Hello World", 254,
+    new List<string> { "Comicità", "Fiction", "Musica", "Just Talking" });
+List<IContenutoRiproducibile> lista = new() { Brano, Podcast };
 
 Brano.Play();
 Brano.Stop();
@@ -16,3 +17,14 @@ Podcast.Stop();
 Podcast.Pause();
 
 Console.WriteLine(Podcast);
+
+foreach(var elemento in lista) {
+    if (elemento is Brano)
+        {
+        Brano ElementoCast=(Brano)elemento;
+        Console.WriteLine(ElementoCast);
+        }else if(elemento is Podcast) {
+        Podcast ElementoCast = (Podcast)elemento;
+        Console.WriteLine(ElementoCast);
+    }
+}
